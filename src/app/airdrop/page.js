3e659216abe28e195962176ad2ac5f963f2a0831
fgymnich-rather <fgymnich@ratherlabs.com>
2024-10-07 13:58:23 -0300

    beauty frontend

--- src/app/airdrop/page.js
+++ src/app/airdrop/page.js
@@ -4,7 +4,7 @@ import { useState, useEffect } from 'react';
 import { ethers } from 'ethers';
 import detectEthereumProvider from '@metamask/detect-provider';
 import abi from './../lib/abi/Airdrop.js';
-import {merkleProof} from './../lib/scripts/merkleTree';
+import { merkleProof } from './../lib/scripts/merkleTree';
 
 export default function AirdropPage() {
   const [provider, setProvider] = useState(null);
@@ -12,21 +12,23 @@ export default function AirdropPage() {
   const [isEligible, setIsEligible] = useState(false);
   const [claimedAmount, setClaimedAmount] = useState('');
   const [statusMessage, setStatusMessage] = useState('');
+  const [statusColor, setStatusColor] = useState('text-white');
+  const [showGlow, setShowGlow] = useState(false);
   const [totalAssigned, setTotalAssigned] = useState(0);
   const [redeemedTokens, setRedeemedTokens] = useState(0);
+  const [isMetaMaskBusy, setIsMetaMaskBusy] = useState(false); 
 
-  const AirdropContractAddress = process.env.NEXT_PUBLIC_AIRDROP_CONTRACT_ADDRESS_LOCAL; // Dirección de tu contrato
+
+  const AirdropContractAddress = process.env.NEXT_PUBLIC_AIRDROP_CONTRACT_ADDRESS_LOCAL;
   const AirdropABI = abi;
 
-  // Array de elegibilidad en el frontend
   const eligibleUsers = [
-    { address: '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266', amount: ethers.parseEther("100") },
-    { address: '0x70997970C51812dc3A010C7d01b50e0d17dc79C8', amount: ethers.parseEther("200") },
-    { address: '0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC', amount: ethers.parseEther("150") },
-    { address: process.env.NEXT_PUBLIC_ADDRESS_METAMASK_TESTING, amount: ethers.parseEther("100") },
-  ]; 
+    { address: '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266', amount: ethers.parseEther('100') },
+    { address: '0x70997970C51812dc3A010C7d01b50e0d17dc79C8', amount: ethers.parseEther('200') },
+    { address: '0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC', amount: ethers.parseEther('150') },
+    { address: process.env.NEXT_PUBLIC_ADDRESS_METAMASK_TESTING, amount: ethers.parseEther('100') },
+  ];
 
-  // Conectar a MetaMask
   const connectWallet = async () => {
     try {
       const provider = await detectEthereumProvider();
@@ -34,7 +36,6 @@ export default function AirdropPage() {
         setProvider(provider);
         const accounts = await provider.request({ method: 'eth_requestAccounts' });
         setAccount(accounts[0]);
-        // Iniciar escucha para cambios de cuenta
         provider.on('accountsChanged', (accounts) => setAccount(accounts[0]));
       } else {
         alert('MetaMask no está instalada. Por favor instala MetaMask.');
@@ -44,49 +45,54 @@ export default function AirdropPage() {
     }
   };
 
-  // Verificar elegibilidad usando el array público
   const verifyEligibility = async () => {
     if (!account) return;
 
-    // Buscar si la cuenta conectada está en el array de elegibles
     const user = eligibleUsers.find(user => user.address.toLowerCase() === account.toLowerCase());
 
     if (user) {
-      setIsEligible(true); 
-      setTotalAssigned(ethers.formatEther(user.amount)); // Convertir de Wei a Ether
-      setStatusMessage('Eres elegible para el airdrop.');
+      setIsEligible(true);
+      setTotalAssigned(ethers.formatEther(user.amount));
+      updateStatusMessage('Eres elegible para el airdrop.', 'success');
     } else {
       setIsEligible(false);
       setTotalAssigned(0);
-      setStatusMessage('No eres elegible para el airdrop.');
+      updateStatusMessage('No eres elegible para el airdrop.', 'error');
     }
   };
 
-  // Función para hacer el claim de los tokens
   const claimTokens = async () => {
     if (!provider || !account || !isEligible || claimedAmount === '' || claimedAmount <= 0) return;
 
-    try { 
+    try {
+      setIsMetaMaskBusy(true);
       const ethersProvider = new ethers.BrowserProvider(provider);
       const signer = await ethersProvider.getSigner();
-      console.log("signer: ",signer.address);  
-
       const airdropContract = new ethers.Contract(AirdropContractAddress, AirdropABI, signer);
 
-      // Generate Merkle Proof 
       const { witnesses, path } = merkleProof(eligibleUsers, { address: account, amount: ethers.parseEther(totalAssigned) });
-
       const tx = await airdropContract.airdrop(witnesses, ethers.parseEther(totalAssigned), ethers.parseEther(claimedAmount), path);
       await tx.wait();
 
-      setStatusMessage(`Has reclamado ${claimedAmount} tokens exitosamente.`);
-      setRedeemedTokens(redeemedTokens+claimedAmount); //sumar la cantidad redeemed
-      setClaimedAmount(''); // Resetear el campo de cantidad
+      updateStatusMessage(`Has reclamado ${claimedAmount} tokens exitosamente.`, 'success');
+      setRedeemedTokens(redeemedTokens + parseFloat(claimedAmount));
+      setClaimedAmount('');
     } catch (error) {
       console.error('Error al reclamar los tokens:', error);
-      alert(`Error: ${error.info.error.message}`);
-      setStatusMessage( `Hubo un error al reclamar los tokens. Razón: ${error.reason}`);
+      updateStatusMessage(`Hubo un error al reclamar los tokens. Razón: ${error.reason}`, 'error');
+    } finally {
+      setIsMetaMaskBusy(false); 
     }
+  };
+
+  const updateStatusMessage = (message, type) => {
+    setStatusMessage(message);
+    setStatusColor(type === 'success' ? 'text-green-500' : 'text-red-500');
+    setShowGlow(true);
+
+    setTimeout(() => {
+      setShowGlow(false); // Quitar el brillo después
+    }, 1000);
   };
 
   useEffect(() => {
@@ -94,48 +100,69 @@ export default function AirdropPage() {
   }, [account]);
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2 bg-gray-100">
-      <h1 className="text-4xl font-bold mb-6">Claim Airdrop</h1>
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-900 text-white p-4 font-[family-name:var(--font-geist-sans)]">
+      <h1 className="text-5xl font-extrabold mb-8 px-4 py-2 rounded-lg text-gradient bg-gradient-to-r from-purple-400 to-blue-500">
+        MTK Claim
+      </h1>
 
-      <div className="mb-4">
+      <div className="mb-8 p-6 bg-gray-800 rounded-lg shadow-lg w-full max-w-lg text-center">
         {!account ? (
           <button
             onClick={connectWallet}
-            className="px-6 py-3 bg-blue-500 text-white font-bold rounded-lg"
+            className="px-6 py-3 bg-blue-600 hover:bg-blue-700 text-white font-bold rounded-lg transition duration-300"
           >
             Conectar MetaMask
           </button>
         ) : (
-          <div className="text-lg font-semibold">Address Conectado: {account}</div>
-        )}
-      </div>
-
-      <div className="mb-4 bg-blue-300 rounded px-2 shadow transition ease-in-out">
-        {account && (
-          <div>
-            <p className="text-xl">{statusMessage}</p>
+          <div className="text-lg rounded font-semibold">
+            <p className="text-green-400">Conectado: {account}</p>
+            <p
+              className={`${statusColor} mt-4 text-lg italic rounded transition-all duration-2000 ${
+                showGlow ? 'animate-pulse shadow-[0_0_20px_currentColor]' : ''
+              }`}
+            >
+              {statusMessage}
+            </p>
           </div>
         )}
       </div>
 
       {isEligible && (
-        <div className="flex flex-col items-center mt-4">
-          <p className="mb-2 text-lg">Tokens Asignados: {totalAssigned}</p>
-          <p className="mb-2 text-lg">Tokens Redimidos: {redeemedTokens}</p>
+        <div className="flex flex-col items-center mt-6 p-6 bg-gray-800 rounded-lg shadow-2xl w-full max-w-xl">
+          <div className="mb-4">
+            <p className="text-2xl text-transparent font-[family-name:var(--font-geist-mono)] bg-clip-text bg-gradient-to-r from-yellow-400 to-orange-500 font-bold shadow-md">
+              Tokens Asignados:
+              <span className="ml-2 px-2 py-1 bg-yellow-500 rounded-lg shadow-lg text-black">
+                {totalAssigned}
+              </span>
+            </p>
+          </div>
+          <div className="mb-4">
+            <p className="text-2xl text-transparent font-[family-name:var(--font-geist-mono)] bg-clip-text bg-gradient-to-r from-yellow-400 to-orange-500 font-bold shadow-md">
+              Tokens Reclamados:
+              <span className="ml-2 px-2 py-1 bg-yellow-500 rounded-lg shadow-lg text-black">
+                {redeemedTokens}
+              </span>
+            </p>
+          </div>
 
-          <label className="mb-2 text-lg">Cantidad a reclamar:</label>
-          <input
-            type="number"
-            value={claimedAmount}
-            onChange={(e) => setClaimedAmount(e.target.value)}
-            className="mb-4 px-4 py-2 border border-gray-300 rounded-lg"
-            placeholder="0.0"
-          />
+          <div className="w-full">
+            <label className="text-xl mb-2 block font-[family-name:var(--font-geist-mono)]">Cantidad a reclamar:</label>
+            <input
+              type="number"
+              value={claimedAmount}
+              onChange={(e) => setClaimedAmount(e.target.value)}
+              className="mb-4 w-full px-4 py-2 bg-gray-700 text-white border font-[family-name:var(--font-geist-mono)] border-gray-600 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+              placeholder="0.0"
+            />
+          </div>
+
           <button
             onClick={claimTokens}
-            className="px-6 py-3 bg-green-500 text-white font-bold rounded-lg shadow-lg"
+            className={ `px-8 py-3 bg-green-500 enabled:hover:bg-green-600 text-white font-bold rounded-lg shadow-lg transition duration-300 dsiabled:bg-slate `}
+            disabled={isMetaMaskBusy}
           >
-            Reclamar Tokens
+            {isMetaMaskBusy ? 'Procesando...' : 'Reclamar Tokens'}
           </button>
         </div>
       )}
--- src/app/layout.js
+++ src/app/layout.js
@@ -11,6 +11,12 @@ const geistMono = localFont({
   variable: "--font-geist-mono",
   weight: "100 900",
 });
+const orbitron = localFont({
+  src: "./fonts/Orbitron.woff2",
+  variable: "--font-orbitron",
+  weight: "100 900",
+});
+
 
 export const metadata = {
   title: "Create Next App",
@@ -21,7 +27,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} ${orbitron.variable} antialiased`}
       >
         {children}
       </body>
