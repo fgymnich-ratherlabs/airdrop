df5ffad99010e694c6604c96d860bfaec869e467
fgymnich-rather <fgymnich@ratherlabs.com>
2024-10-10 17:56:24 -0300

    redirection

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    redirects: async () => {
+        return [
+          {
+            source: '/',
+            destination: '/airdrop',
+            permanent: false, //this redirect is not cached by the browser
+          },
+        ];
+      }, 
+};
 
 export default nextConfig;
